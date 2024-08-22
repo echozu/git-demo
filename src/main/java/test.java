f35e6c600819553eda41704725179a0d0df040e6
echozu <3158779947@qq.com>
2024-08-23 01:55:03 +0800

    third commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hhha");
         System.out.println("git2");
+        System.out.println("git3");
     }
 }
